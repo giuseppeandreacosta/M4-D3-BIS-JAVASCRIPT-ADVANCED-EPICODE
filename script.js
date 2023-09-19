af67ce12b34581cef84286f976515d4fa7de551c
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-09-19 23:17:47 +0200

    resolve js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const apiKey = "IzjqfEL1ITBGQrac2m7bjGM4PWARcr7zfCIO7PlON2DnpsZo8MSvC6Lu";
 const perPage = 15;
 let currentPage = 1;
 let searchTerm = "";
-
+console.log(perPage)
 function getImages(url) {
     fetch(url, {
         method: 'GET',
@@ -64,15 +64,3 @@ loadMoreBtn.addEventListener("click", () => {
 getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
 
 
-
-/* 
-
-1. **Variabili**: All'inizio del codice, vengono dichiarate alcune variabili. Queste includono riferimenti a elementi del DOM come `imageWrapper`, `searchInput` e `loadMoreBtn`, così come variabili per gestire la paginazione (`currentPage`) e la ricerca (`searchTerm`).
-
-2. **Funzione getImages**: Questa funzione esegue una richiesta GET all'API di Pexels utilizzando l'URL fornito come parametro. Utilizza l'API key per autenticarsi con il servizio. Una volta ricevuti i dati, li passa alla funzione `generateHTML`.
-
-3. **Funzione generateHTML**: Questa funzione prende un array di immagini e genera l'HTML per visualizzarle sulla pagina. Utilizza il metodo `map` per creare un array di stringhe HTML, una per ogni immagine, e poi aggiunge queste stringhe all'elemento `imageWrapper`.
-
-4. **Event Listener**: Ci sono due event listener alla fine del codice. Il primo si attiva quando l'utente preme il tasto "Enter" nel campo di input di ricerca, eseguendo una nuova ricerca con il termine di ricerca dell'utente. Il secondo si attiva quando l'utente clicca sul pulsante "Load More", caricando più immagini.
-
-5. **Chiamata Iniziale**: Infine, il codice chiama la funzione `getImages` con un URL predefinito per caricare le prime immagini non appena lo script viene eseguito*/
